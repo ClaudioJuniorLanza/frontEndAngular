6b39530e0a751828e3d1fffb20ad93a6f43c9dab
Claudio Lanca <claudiojuniorlanza@gmail.com>
2020-03-18 14:53:26 -0300

    primeiro commit do projeto

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,46 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { LancamentosGridComponent } from './lancamentos-grid/lancamentos-grid.component';
+import { LancamentosCadastroComponent } from './lancamentos-cadastro/lancamentos-cadastro.component';
+import { LancamentosPesquisaComponent } from './lancamentos-pesquisa/lancamentos-pesquisa.component';
+import { PessoasLancamentoComponent } from './pessoas-lancamento/pessoas-lancamento.component';
+import { PessoasCadastroComponent } from './pessoas-cadastro/pessoas-cadastro.component';
+import { PessoasGridComponent } from './pessoas-grid/pessoas-grid.component';
+import { NavbarComponent } from './navbar/navbar.component';
+
+
+import {InputTextModule} from 'primeng/inputtext';
+import {ButtonModule} from 'primeng/button';
+import {TableModule} from 'primeng/table';
+import {TooltipModule} from 'primeng/tooltip';
+import {InputTextareaModule} from 'primeng/inputtextarea';
+import {CalendarModule} from 'primeng/calendar';
+import {SelectButtonModule} from 'primeng/selectbutton';
+import {DropdownModule} from 'primeng/dropdown';
+import { InputMaskModule } from 'primeng/inputmask';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FormsModule } from '@angular/forms';
+
+import { CurrencyMaskModule } from 'ng2-currency-mask';
+import { MessageComponent } from './message/message.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LancamentosGridComponent,
+    LancamentosCadastroComponent,
+    LancamentosPesquisaComponent,
+    PessoasLancamentoComponent,
+    PessoasCadastroComponent,
+    PessoasGridComponent,
+    NavbarComponent,
+    MessageComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule, BrowserAnimationsModule, InputTextModule, ButtonModule, TableModule,
+    TooltipModule, InputTextareaModule, CalendarModule, SelectButtonModule, DropdownModule,
+    CurrencyMaskModule, FormsModule, InputMaskModule
   ],
   providers: [],
   bootstrap: [AppComponent]
